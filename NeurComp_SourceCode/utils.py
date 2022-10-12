a01921f4497f517fbc42a2f545f31139b8ec38d7
Robert Michael Sales <44003192+RobertMichaelSales@users.noreply.github.com>
2022-10-12 17:39:36 +0100

    Commenting utils.py

--- NeurComp_SourceCode/utils.py
+++ NeurComp_SourceCode/utils.py
@@ -6,23 +6,29 @@ import torch as th
 
 from pyevtk.hl import imageToVTK
 
-'''
---- Single scalar field ---
-'''
+#==============================================================================
+# Defines a function (not used anywhere)
 
 def field_and_grad_from_net(dataset, net, is_cuda, tiled_res=32):
+    
     target_res = dataset.vol_res
     full_vol = th.zeros(target_res)
     target_res_pos = list(dataset.vol_res)
     target_res_pos.append(net.d_in)
     full_grad = th.zeros(target_res_pos)
+    
     for xdx in np.arange(0,target_res[0],tiled_res):
+        
         x_begin = xdx
         x_end = xdx+tiled_res if xdx+tiled_res <= target_res[0] else target_res[0]
+        
         for ydx in np.arange(0,target_res[1],tiled_res):
+            
             y_begin = ydx
             y_end = ydx+tiled_res if ydx+tiled_res <= target_res[1] else target_res[1]
+            
             for zdx in np.arange(0,target_res[2],tiled_res):
+                
                 z_begin = zdx
                 z_end = zdx+tiled_res if zdx+tiled_res <= target_res[2] else target_res[2]
 
@@ -34,8 +40,11 @@ def field_and_grad_from_net(dataset, net, is_cuda, tiled_res=32):
                 max_bounds = dataset.min_bb + max_alpha_bb*(dataset.max_bb-dataset.min_bb)
 
                 tile_positions = dataset.scales.view(1,1,1,3)*dataset.tile_sampling(min_bounds,max_bounds,tile_resolution)
+                
                 if is_cuda:
+                    
                     tile_positions = tile_positions.unsqueeze(0).cuda()
+                    
                 tile_positions.requires_grad = True
                 tile_vol = net(tile_positions.unsqueeze(0)).squeeze(0).squeeze(-1)
 
@@ -44,46 +53,73 @@ def field_and_grad_from_net(dataset, net, is_cuda, tiled_res=32):
                 full_grad[x_begin:x_end,y_begin:y_end,z_begin:z_end] = vol_grad.detach().cpu()
 
                 full_vol[x_begin:x_end,y_begin:y_end,z_begin:z_end] = tile_vol.detach().cpu()
-            #
-        #
-    #
+
     return full_vol,full_grad
-#
+
+#==============================================================================
+# Defines a function (...)
 
 def field_from_net(dataset, net, is_cuda, tiled_res=32, verbose=False):
+    
+    # Set 'target_res' to the shape of the input volume, i.e [150,150,150]
     target_res = dataset.vol_res
+    
+    # Set 'full_vol' to be the shape of the input volume filled with zeros
     full_vol = th.zeros(target_res)
+    
+    # Iterate through the x-slices (interval = tiled_res)
+    # This gives -> i.e. [0,32,64,96,128] not [0,32,64,96,128,150]
     for xdx in np.arange(0,target_res[0],tiled_res):
-        if verbose:
-            print('x',xdx,'/',target_res[0])
+        
+        if verbose:print('x',xdx,'/',target_res[0])
+            
         x_begin = xdx
         x_end = xdx+tiled_res if xdx+tiled_res <= target_res[0] else target_res[0]
+        
+        # Iterate through the y-slices  (interval = tiled_res)
+        # This gives -> i.e. [0,32,64,96,128] not [0,32,64,96,128,150]
         for ydx in np.arange(0,target_res[1],tiled_res):
+            
             y_begin = ydx
             y_end = ydx+tiled_res if ydx+tiled_res <= target_res[1] else target_res[1]
+            
+            # Iterate through the z-slices  (interval = tiled_res)
+            # This gives -> i.e. [0,32,64,96,128] not [0,32,64,96,128,150]
             for zdx in np.arange(0,target_res[2],tiled_res):
+                
                 z_begin = zdx
                 z_end = zdx+tiled_res if zdx+tiled_res <= target_res[2] else target_res[2]
 
+                # Calculate the distance between successive points. This equals
+                # 'tiled_res' except for the final iteration, which is smaller.
+                # i.e. "32, 32, 32, 32, 22"                
                 tile_resolution = th.tensor([x_end-x_begin,y_end-y_begin,z_end-z_begin],dtype=th.int)
 
+                # Determines normalised coordinates of cubes within the volume 
+                # such that the entire volume is higlighted once and only once 
                 min_alpha_bb = th.tensor([x_begin/(target_res[0]-1),y_begin/(target_res[1]-1),z_begin/(target_res[2]-1)],dtype=th.float)
                 max_alpha_bb = th.tensor([(x_end-1)/(target_res[0]-1),(y_end-1)/(target_res[1]-1),(z_end-1)/(target_res[2]-1)],dtype=th.float)
+                
+                # Determines the indices (as floats) of the cube corners of the
+                # above-mentioned cubes.
                 min_bounds = dataset.min_bb + min_alpha_bb*(dataset.max_bb-dataset.min_bb)
-                max_bounds = dataset.min_bb + max_alpha_bb*(dataset.max_bb-dataset.min_bb)
+                max_bounds = dataset.min_bb + max_alpha_bb*(dataset.max_bb-dataset.min_bb)                
 
                 with th.no_grad():
+                    
                     tile_positions = dataset.scales.view(1,1,1,3)*dataset.tile_sampling(min_bounds,max_bounds,tile_resolution)
+                    
                     if is_cuda:
+                        
                         tile_positions = tile_positions.unsqueeze(0).cuda()
+                        
                     tile_vol = net(tile_positions.unsqueeze(0)).squeeze(0).squeeze(-1)
                     full_vol[x_begin:x_end,y_begin:y_end,z_begin:z_end] = tile_vol.cpu()
-                #
-            #
-        #
-    #
+
     return full_vol
-#
+
+#==============================================================================
+# Defines a function (...)
 
 def tiled_net_out(dataset, net, is_cuda, gt_vol=None, evaluate=True, write_vols=False, filename='vol'):
     net.eval()
